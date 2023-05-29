eb4203ff2d88e45ee588032f4ec108aa91c37b9f
frodo <frodo.bupo@yahoo.de>
2023-05-29 14:39:16 +0200

    Problem59 gelöst

--- src/problems/p051TOp060/p059/Problem59.kt
+++ src/problems/p051TOp060/p059/Problem59.kt
@@ -1,35 +1,71 @@
 package problems.p051TOp060.p059
 
 import util.IProblem
-import java.math.BigDecimal
+import java.io.File
 import java.math.BigInteger
-import kotlin.experimental.xor
 import kotlin.streams.toList
 
 class Problem59:IProblem{
 
+   private val bitLenght = 8
+
+   override fun getSolution(): String {
+      return "129448"
+   }
 
    override fun solve(): String {
 
-      val test = "HALLO"
-      val testListOfBytes = test.chars().toList().map { e -> BElement(e)};
-      val key:List<Int> = "xx".chars().toList();
+      var readableString = ""
+      val cypherText = File(javaClass.getResource("0059_cipher.txt").file).readText()
+      val cypherBStrings = cypherText.split(",").map { e-> this.intToBString(e.toInt()) }.toList()
 
-      println("Testword: $test");
-      println("Code key: "+ key.map { e->Char(e) }).toString()
-      println("Testword singe bytes: "+testListOfBytes.joinToString("") { e -> e.bString });
-      println("Code key bytes: "+ key.map { e->e.toString(2) })
+      for (key in permuKeys()) {
 
-      val totalTestWordBytes = testListOfBytes.joinToString("") { e -> e.bString };
-      val totalKeyBytes =
-         expandKeyBytes(key.map { e -> e.toString(2) }.toList().joinToString(""), totalTestWordBytes.length);
+         readableString = getRaedableString(key, cypherBStrings)
 
-      println("Testword full bytes: $totalTestWordBytes")
-      println("Code key full bytes: $totalKeyBytes")
+         if(readableString.contains("the ", true) && readableString.contains("is ", true)){
+//            println(readableString)
+            break
+         }
+      }
 
+      return readableString.chars().sum().toString()
+   }
 
+   private fun getRaedableString(key:String, cypherBStrings:List<String>):String  {
+      val bkey = key.chars().toList().joinToString("") { e -> this.intToBString(e) }
+      val totalCypherBytes = cypherBStrings.joinToString("")
 
-      return ""
+      val decryptRaw = xor(totalCypherBytes, expandKeyBytes(bkey, totalCypherBytes.length))
+      return convertToStrByteToString(decryptRaw, cypherBStrings.size)
+   }
+
+   private fun permuKeys():List<String> {
+      val pool = "abcdefghijklmnopqrstuvwxyz".split("").filter { e-> e.isNotBlank() }.toList()
+      val returnList = mutableListOf<String>()
+
+      for (i in pool.indices) {
+         for (j in pool.indices) {
+            for (k in pool.indices) {
+               returnList.add(pool.get(i)+pool.get(j)+pool.get(k))
+            }
+         }
+      }
+
+      return  returnList
+   }
+
+   private fun convertToStrByteToString(strByte:String, chunks: Int):String{
+
+      var runIndex = 0
+      var wordString = "";
+
+      for(i in 0 until  chunks){
+         wordString+=Char(BigInteger(strByte.substring(runIndex, runIndex+bitLenght), 2).toInt()).toString()
+         runIndex+=bitLenght
+      }
+
+      return wordString;
    }
 
    private fun expandKeyBytes(keyBytes:String, targetLength:Int):String{
@@ -43,10 +79,7 @@ class Problem59:IProblem{
       return expandKey.substring(0,targetLength)
    }
 
-   // Own xor is needed
+   private fun xor(strBytes1:String, strBytes2:String) = BigInteger(strBytes1,2).xor(BigInteger(strBytes2,2)).toString(2).padStart(strBytes1.length,'0')
 
-   private class BElement(val bInt: Int){
-      val bString = bInt.toString(2)
-      val length:Int = bString.length
-   }
+   private fun intToBString(bInt: Int)= bInt.toString(2).padStart(bitLenght,'0')
 }
