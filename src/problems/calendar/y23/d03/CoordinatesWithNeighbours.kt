da0966d98834a6b7431ba7879dc2b507875e8c6f
Maximilian Scholz <maximilian.scholz@unitb.de>
2024-02-22 16:27:46 +0100

    Seperate var if the neighbours are already calculated

--- src/problems/calendar/y23/d03/CoordinatesWithNeighbours.kt
+++ src/problems/calendar/y23/d03/CoordinatesWithNeighbours.kt
@@ -2,6 +2,7 @@ package problems.calendar.y23.d03
 
 class CoordinatesWithNeighbours(val x: Int, val y :Int, private val dataLocator: DataLocator){
    val rawValue = dataLocator.get(x,y)
+   private var neighbours = listOf<CoordinatesWithNeighbours>()
    private val neighbourCoordinates =
       listOf(Pair(x-1,y),
          Pair(x+1,y),
@@ -12,9 +13,14 @@ class CoordinatesWithNeighbours(val x: Int, val y :Int, private val dataLocator:
          Pair(x+1,y-1),
          Pair(x-1,y+1))
 
-   fun getNeighbours() = neighbourCoordinates
-      .map {  CoordinatesWithNeighbours(it.first, it.second, dataLocator) }
-      .filter { e -> e.rawValue != null }
+   fun getNeighbours(): List<CoordinatesWithNeighbours> {
+      if(neighbours.isEmpty()){
+         neighbours = neighbourCoordinates
+            .map { CoordinatesWithNeighbours(it.first, it.second, dataLocator) }
+            .filter { e -> e.rawValue != null }
+      }
+      return neighbours
+   }
 
    fun isDigit() = rawValue?.map { it.isDigit() }?.first()?: false
    fun isSign() = !isDigit() && rawValue != "."
